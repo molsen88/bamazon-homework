28bb8c525797bbf0200d7e05ab506232445aabd0
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-04-29 22:19:48 -0600

    working homework

--- Assets/Javascript/bamazonCustomer.js
+++ Assets/Javascript/bamazonCustomer.js
@@ -29,127 +29,56 @@ function start() {
 
 function choice() {
     inquirer
-        .prompt( {
+        .prompt( [{
             name: "purchase",
             message: "What item(#) would you like to buy?",
-            type: "list",
-            choices: ['W10', 'W11', 'W12', 'W13', 'W14', 'W15', 'W16', 'W17', 'W18', 'W19']
-        } ).then( function ( res ) {
-            var item = res.purchase;
-            console.log( item )
-
-
-            switch ( res.purchase ) {
-                case "W10":
-                    checkInventory();
-                    break;
-
-                case "W11":
-                    checkInventory();
-                    break;
-
-                case "W12":
-                    checkInventory();
-                    break;
-
-                case "W13":
-                    checkInventory();
-                    break;
-
-                case "W14":
-                    checkInventory();
-                    break;
-
-                case "W15":
-                    checkInventory();
-                    break;
-
-                case "W16":
-                    checkInventory();
-                    break;
-
-                case "W17":
-                    checkInventory();
-                    break;
-
-                case "W18":
-                    checkInventory();
-                    break;
-
-                case "W19":
-                    checkInventory();
-                    break;
-            }
-        } )
-}
-
-
-function checkInventory() {
-    inquirer
-        .prompt( {
+            type: "input"
+        },
+        {
             name: "amount",
             message: "How many would you like to purchase?",
             type: "input"
-        } ).then( function ( answer ) {
-            connection.query( "SELECT*FROM products WHERE ?", { amount: answer.amount }, function ( err, res ) {
-                // console.log( answer.amount )
-                var quantity = answer.amount;
 
-                console.log( quantity );
+        }] ).then( function ( res ) {
+            var item = res.purchase;
+            var quantity = res.amount;
+            console.log( item )
+            console.log( quantity )
+
+            var query = 'SELECT*FROM products WHERE item_id '
+            connection.query( query, { item_id: item }, function ( err, data ) {
+                if ( err ) throw err;
+
+                if ( data.length === 0 ) {
+                    console.log( 'ERROR: Invalid Item ID. Please select a valid Item ID.' );
+                    displayInventory();
+                } else {
+                    var productData = data[0];
+                    // quantity is available
+                    if ( quantity <= productData.stock_quantity ) {
+                        console.log( 'Congratulations, the product you requested is in stock! Placing order!' );
+                        // update the table
+                        var updateQuery = 'UPDATE products SET stock_quantity = ' + ( productData.stock_quantity - quantity ) + ' WHERE item_id = ' + item;
+
+                        connection.query( updateQuery, function ( err, data ) {
+                            if ( err ) throw err;
+                            console.log( 'Your oder has been placed! Your total is $' + productData.price * quantity + ". Thanks for your purchase!" );
+
+                            console.log( "\n---------------------------------------------------------------------\n" );
+                            // End the database connection
+                            connection.end();
+                        } )
+                    } else {
+                        console.log( 'Sorry, there is not enough product in stock, your order can not be placed as is. You will need to change your order' );
+                        console.log( "\n---------------------------------------------------------------------\n" );
+                        choice();
+                    }
+                }
             } )
-
-
-            availableProduct();
         } )
 }
 
-//     var query = 'select * from products where item_id = "" ';
-//     connection.query( query, function ( err, data ) {
-//         if ( err ) throw err;
-//         for ( var x = 0; x < data.length; x++ ) {
-//             console.log( data[x].item_id );
-//         }
-//     } )
-// }
-
-function availableProduct() {
-    var query = "SELECT*FROM products WHERE stock_quantity  ";
-    connection.query( query, { purchase: res.purchase }, function ( err, data ) {
-        if ( err ) throw err;
-        for ( var i = 0; i < data.length; i++ ) {
-            if ( item === data[i].item_id ) {
-                console.log( "works" )
-            }
-            console.log( data[i].item_id, data[i].stock_quantity );
-        }
-
-
-    } )
-}
-
-
-// console.log( item );
-// console.log( quantity )
 
 
 
 
-
-
-    // Respond to the input by user and check for avaliable product
-
-    // function availableProduct() {
-    //     query = "SELECT item_id FROM products WHERE stock_quantity ";
-    //     connection.query( query, function ( err, data ) {
-    //         var numberPurchase = 'amount';
-    //         if ( err ) throw err;
-    //         for ( var i = 0; i < data.length; i++ ) {
-    //             if ( numberPurchase > data.stock_quantity ) {
-    //                 console.log( "Product is available" )
-    //             }
-    //             else {
-    //                 console.log( "insufficient quantity" )
-    //             }
-    //         }
-    //     } )
-    // }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bamazon-homework
+
+The purpose of this project is to get data from a SQL table and display it to the user.
+
+Once the user has the data, they are then asked what item they'd like to purchase and the amount of that item.
+
+The users input is saved and used to determine if it is available.
+
+If it isn't available, the user muse change their amount.
+
+If it is available, the user will have their order placed and given a total.
+
+![Node bamazon giphy](link to image)
